b5162059c3b60473da96c6550ab32e2096f240a2
Israel CM <isracat.32@gmail.com>
2020-01-30 23:55:48 -0400

    Remove dummy expenses data

--- src/app.js
+++ src/app.js
@@ -9,16 +9,7 @@ import './styles/base/_base.scss';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
-import { addExpense } from './store/actions/expenses';
-
 const store = configureStore();
-
-
-const expenseOne = store.dispatch( addExpense({ description: 'Rent rent', amount: 100, createdAt: 1000 }));
-const expenseTwo = store.dispatch( addExpense({ description: 'Meat', amount: 140, createdAt: -1000 }));
-const expenseThree = store.dispatch( addExpense({ description: 'Chocolate', amount: 160, createdAt: 15000 }));
-console.log(store.getState());
-
 
 const jsx = (
     <Provider store={ store }>
